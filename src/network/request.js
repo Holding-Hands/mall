1c6b16aa94dc24d1d766d420251bf7def8eb1ad1
“Holding-Hands” <1760135477@qq.com>
2020-04-13 20:46:18 +0800

    '优化航班卡片列表细节'

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 export function request (config){
      const instance1 = axios.create({
         // baseURL : "http://123.207.32.32:8000/api/wh" 
-        baseURL : "http://106.54.54.237:8000/api/wh" 
+      //   baseURL : "http://106.54.54.237:8000/api/wh" 
+      // baseURL : "http://106.54.54.237:8000/api/wh" 
+     baseURL:' http://123.207.32.32:8000/api/h3'
      })
 
     return instance1(config)
